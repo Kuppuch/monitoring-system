b026085d63729d496c64ce70f2cba99285b53e0e
Kuppuch <kuppuchr@gmail.com>
2023-03-22 22:15:16 +0300

    починено отображение задач

--- pages/js/aside.js
+++ pages/js/aside.js
@@ -4,12 +4,10 @@ const closeBtn = document.querySelector("#close-btn");
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block'
-    console.log('aaaaaaaaaa')
 })
 
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none'
-    console.log('aaaaaaaaaa')
 })
 
 // addEventListener("load", (event) => {
@@ -19,7 +17,6 @@ const sidebar = document.querySelector('.sidebar')
 let links = sidebar.children
 const path = url.pathname.replace(/.$/, "")
 for (let i = 0; i < links.length; i++) {
-    console.log(links[i])
     if (links[i].href.includes(path) && path.length > 1) {
         links[i].classList.add('active')
         break
